117cf5e57b2d6263477c367d5868c4e4ede241b7
Tom Ziegler <tom.ziegler1@gmx.de>
2020-04-11 16:59:35 +0200

    release 1.01-20apr11

--- common-includes.php
+++ common-includes.php
@@ -33,9 +33,9 @@
    *********************************** -->
 
 
-    <script src="/src/releases/1.01-dec10/js/init.js"></script>
-    <script src="/src/releases/1.01-dec10/js/common.js"></script>
-    <script src="/src/releases/1.01-dec10/js/footer.js"></script>
+    <script src="/src/releases/1.01-20apr11/js/init.js"></script>
+    <script src="/src/releases/1.01-20apr11/js/common.js"></script>
+    <script src="/src/releases/1.01-20apr11/js/footer.js"></script>
 
 
     <!--
@@ -51,10 +51,10 @@
       ***********************************
       ** css files
       *********************************** -->
-    <link rel="stylesheet" type="text/css" href="/src/releases/1.01-dec10/css/preloader.css"/>
-    <link rel="stylesheet" type="text/css" href="/src/releases/1.01-dec10/css/lssc_loader.css" />
+    <link rel="stylesheet" type="text/css" href="/src/releases/1.01-20apr11/css/preloader.css"/>
+    <link rel="stylesheet" type="text/css" href="/src/releases/1.01-20apr11/css/lssc_loader.css" />
     <!--
      ***********************************
      ** no js fallback
      *********************************** -->
-    <noscript><link rel="stylesheet" type="text/css" href="/src/releases/1.01-dec10/css/no-js.css" /></noscript>
+    <noscript><link rel="stylesheet" type="text/css" href="/src/releases/1.01-20apr11/css/no-js.css" /></noscript>

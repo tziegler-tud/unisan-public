bb8a90f34167a096a9cc88be15ef6dac79cc929e
Tom Ziegler <tom.ziegler1@gmx.de>
2020-06-09 14:30:23 +0200

    added menu entry + small src fixed

--- src/js/index/init.js
+++ src/js/index/init.js
@@ -2,23 +2,7 @@ var lidlRTO;
 
 $(document).ready (function () {
 
-        var Hammer = window.Hammer;
-
-
-        var token = lidlRTO.objectManager.createNewObjectToken();
-        const imgBox01 = new lidl.Imagebox("lidl-imagebox01", token);
-        lidlRTO.objectManager.addObject(imgBox01, token);
-
-        var mc = new Hammer.Manager(imgBox01.getContainerDomElement());
-        mc.add(new Hammer.Swipe({direction: Hammer.DIRECTION_HORIZONTAL, threshold: 30}));
-        mc.on('swipeleft', function () {
-            console.log("swipe left detected");
-            imgBox01.nextImage();
-        });
-        mc.on('swiperight', function () {
-            console.log("swipe right detected");
-            imgBox01.prevImage();
-        });
+        e
 
 
 
